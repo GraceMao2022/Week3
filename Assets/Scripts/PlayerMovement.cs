eb7fcb51b355db2b4cce65c6d8c9802ac89b6df2
GinaMackay2014 <113387926+GraceMao2022@users.noreply.github.com>
2025-02-05 18:04:16 -0800

    added week 4 stuff

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,17 +8,24 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] float jumpHeight = 3f;
     float direction = 0;
     bool isGrounded = false;
+    bool isFacingRight = true;
+
+    Animator anim;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Move(direction);
+
+        if ((isFacingRight && direction == -1) || (!isFacingRight && direction == 1))
+            Flip();
     }
 
     void OnMove(InputValue value)
@@ -30,6 +37,7 @@ public class PlayerMovement : MonoBehaviour
     void Move(float dir)
     {
         rb.linearVelocity = new Vector2(dir * speed, rb.linearVelocity.y);
+        anim.SetBool("isRunning", dir != 0);
     }
 
     void OnJump()
@@ -69,5 +77,13 @@ public class PlayerMovement : MonoBehaviour
         {
             isGrounded = false;
         }
+    }
+
+    private void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        Vector3 newLocalScale = transform.localScale;
+        newLocalScale.x *= -1f;
+        transform.localScale = newLocalScale;
     }
 }
